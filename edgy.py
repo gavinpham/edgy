f5faa375e4a3ef65f0602a62f96d037604dcbe1c
gavinpham <gpham@smu.edu>
2016-06-12 19:19:25 +0900

    Added more tests/looping tests

--- edgy.py
+++ edgy.py
@@ -1,18 +1,26 @@
 import cv2
 import numpy as np
-img = cv2.imread('/Users/Gavin/edgy/20160606_170352.jpg')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,50,150,apertureSize = 3)
-minLineLength = 5000
-maxLineGap = 10
-lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+import os
 
-print len(lines)
+for i in os.listdir(os.getcwd()):
+	if i.endswith(".jpg"): 
+		print i
+		img = cv2.imread('/Users/Gavin/edgy/' + i)
+		gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+		edges = cv2.Canny(gray,50,150,apertureSize = 3)
+		minLineLength = 200
+		maxLineGap = 10
+		lines = cv2.HoughLinesP(edges,1,np.pi/180,100,0,minLineLength,maxLineGap)
 
-#Loop through all found lines in the image
-#Yeah it's gonna be insane
-for line in lines:
-	for x1,y1,x2,y2 in line:
-		cv2.line(img,(x1,y1),(x2,y2),(0,0,255),5)
+		print len(lines)
 
-cv2.imwrite('/Users/Gavin/edgy/20160606_170352_2.jpg',img)
+		#Loop through all found lines in the image
+		#Yeah it's gonna be insane
+		for line in lines:
+			for x1,y1,x2,y2 in line:
+				cv2.line(img,(x1,y1),(x2,y2),(0,0,255),5)
+
+		cv2.imwrite('/Users/Gavin/edgy/output/' + i,img)
+		continue
+	else:
+		continue
